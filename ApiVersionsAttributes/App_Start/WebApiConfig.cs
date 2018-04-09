b27572de5a29c680d96d5379be70456eef398b01
Örjan Eriksson <orjan.eriksson@squeed.com>
2018-04-09 17:10:59 +0200

    Added simple attributes based API.

---
+++ ApiVersionsAttributes/App_Start/WebApiConfig.cs
@@ -0,0 +1,15 @@
+﻿namespace ApiVersionsAttributes
+{
+    using System.Web.Http;
+
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+        }
+    }
+}
---
+++ ApiVersionsAttributes/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿namespace ApiVersionsConvention.Controllers
+{
+    using Domain.Product;
+    using System.Web.Http;
+
+    [RoutePrefix("api/products")]
+    public class ProductsController : ApiController
+    {
+        private readonly ProductRepository _productRepository;
+
+        public ProductsController()
+            : this(new ProductRepository())
+        {
+        }
+
+        public ProductsController(ProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public Product GetProduct(uint id)
+        {
+            return _productRepository.GetProduct(id);
+        }
+    }
+}
